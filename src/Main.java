2a1388f514a795bb15fc5633ead43cd7740b45d3
angusssyyy <angusw2026@student.cis.edu.hk>
2023-09-13 09:48:59 +0800

    1.1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
 import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int age;
+        String firstName;
+        String favoriteFood;
 
+        System.out.println("Please enter your age:");
+        age = scanner.nextInt();
+        scanner.nextLine(); // Consume the remaining newline character
 
+        System.out.println("Please enter your first name:");
+        firstName = scanner.nextLine();
 
+        System.out.println("Please enter your favorite food:");
+        favoriteFood = scanner.nextLine();
 
+        System.out.println("First name: " + firstName + "\nAge: " + age + "\nFavorite food: " + favoriteFood);
     }
-
 }
